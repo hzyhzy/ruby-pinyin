92d7798c7d47e09d8ce90628fa46177aa11430c9
hzy <18670314023@163.com>
2020-02-11 12:14:37 +0800

    fix

--- lib/ruby-pinyin-ez/backend.rb
+++ lib/ruby-pinyin-ez/backend.rb
@@ -1,7 +1,7 @@
 module PinYin
   module Backend
-    autoload :Simple, 'ruby-pinyin/backend/simple'
-    autoload :MMSeg,  'ruby-pinyin/backend/mmseg'
-    autoload :EZSeg,  'ruby-pinyin/backend/ezseg'
+    autoload :Simple, 'ruby-pinyin-ez/backend/simple'
+    autoload :MMSeg,  'ruby-pinyin-ez/backend/mmseg'
+    autoload :EZSeg,  'ruby-pinyin-ez/backend/ezseg'
   end
 end
